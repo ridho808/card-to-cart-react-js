f86e4a61940a61c8a98298062b7dab176bfc167c
RIDHO <90787959+ridho808@users.noreply.github.com>
2022-08-30 09:23:47 +0700

    Update Card.jsx

--- src/component/card/Card.jsx
+++ src/component/card/Card.jsx
@@ -13,7 +13,7 @@ export default class Card extends Component {
                 className="w-[200px] flex flex-col items-center my-3 rounded-b-xl shadow-xl" 
                 data-aos="fade-up" 
                 data-aos-duration="1500">
-                    <img src={res.images} alt="SHOES" className='object-contain w-[150px] h-[200px] bg-white'/>
+                    <img src={res.images} alt="SHOES" className='object-contain w-[200px] h-[200px] bg-white'/>
                     <div className='bg-gray-300 w-full h-full flex flex-col '>
                       <h3 className='font-semibold'>{res.title}</h3>
                       <span className='w-[60px] h-[30px] text-center border-2 border-gray-900 rounded-2xl'>
